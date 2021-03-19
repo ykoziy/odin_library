6e0b038c0ecf783fa9c49053e7af28e3b9e06ae0
Yuriy Koziy <yukoziy@gmail.com>
2021-03-18 22:33:04 -0400

    Added basic code

--- script/library.js
+++ script/library.js
@@ -1,1 +1,26 @@
-console.log("it's alive!!!");
+let myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+Book.prototype.getIsRead = function() {
+    return this.isRead;
+}
+
+Book.prototype.setIsRead = function(readStatus) {
+    this.isRead = readStatus;
+}
+
+Book.prototype.info = function() {
+    const read = "already read";
+    const unread = "not read yet"
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? read : unread}`;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}

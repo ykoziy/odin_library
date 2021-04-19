81b22d7c54187691a7a6ef68429c32d63bb69497
Yuriy Koziy <yukoziy@gmail.com>
2021-04-19 08:54:33 -0400

    Refactored Book to use ES6 class.

--- script/library.js
+++ script/library.js
@@ -1,20 +1,22 @@
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-}
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
 
-Book.prototype.toggleIsRead = function() {
-    this.isRead = !this.isRead;
-}
+    toggleIsRead() {
+        this.isRead = !this.isRead;       
+    }
 
-Book.prototype.info = function() {
-    const read = "already read";
-    const unread = "not read yet"
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? read : unread}`;
+    info() {
+        const read = "already read";
+        const unread = "not read yet"
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? read : unread}`;        
+    }
 }
 
 function addBooksToLibrary() {
